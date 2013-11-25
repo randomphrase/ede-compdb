c23b77cb0b07e110c7108d428e75ab6c3aef4b3f
Alastair Rankine <alastair@girtby.net>
2013-11-24 22:10:09 -0600

    More hacking, trying to test that file is parsed correctly

--- ede-compdb-test.el
+++ ede-compdb-test.el
@@ -8,13 +8,16 @@
     (unwind-protect
         (with-current-buffer (get-buffer-create "*ede-compdb-test*")
           (let ((testdir (expand-file-name "test" (when load-file-name (file-name-directory load-file-name))))
-              (default-directory builddir))
+                (default-directory builddir))
             (erase-buffer)
-            (call-process "cmake" nil t t "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" testdir)
-            ;; TODO: fail if cmake fails?
-            (funcall body testdir builddir)
+            (let ((ret (call-process "cmake" nil t t "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" testdir)))
+              (when (> 0 ret)
+                (error "Error running CMake: error %d" ret)))
+            (funcall body (file-name-as-directory testdir) builddir)
             ))
-      (delete-directory builddir t))))
+      (progn
+        (delete-directory builddir t)
+        (ede-flush-deleted-projects)))))
 
 (ert-deftest make-compdb-project ()
   "Tests the parsing of a CMake-generated compile_commands.json file to construct an ede-compdb-project"
@@ -22,16 +25,38 @@
   (build-directory-fixture
    (lambda (testdir builddir)
      (should (file-exists-p builddir))
-     (let ((proj (ede-compdb-project "TESTPROJ" :file (expand-file-name "compile_commands.json" builddir))))
-       (should (hash-table-p (oref proj compdb)))
-       (should (< 0 (hash-table-count (oref proj compdb))))
-       (should (gethash (expand-file-name "hello.cpp" testdir) (oref proj compdb))))
-     )))
+
+     (let ((proj (ede-add-project-to-global-list
+                  (ede-compdb-project "TESTPROJ" :directory testdir :file (expand-file-name "compile_commands.json" builddir))))
+           (hellocpp (expand-file-name "hello.cpp" testdir)))
+
+       ;; Basic sanity checks on the project itself
+       (should (eq proj (ede-directory-get-open-project testdir)))
+       (should (gethash hellocpp (oref proj compdb)))
+
+       ;; Now we'll open a source file and check that it is parsed correctly
+       (let ((buf (find-file-noselect hellocpp)))
+         (unwind-protect
+             (with-current-buffer buf
+               ;; Should have set up the current project and target
+               (should (eq proj (ede-current-project)))
+               (should (oref ede-object compilation))
+
+               ;; Should have been parsed
+               (should (semantic-active-p))
+               (should (not semantic-parser-warnings))
+
+               ;; All includes should be parsed
+               (dolist (inc (semantic-find-tags-by-class 'include))
+                       (should (semantic-dependency-tag-file inc)))
+               )
+           (kill-buffer buf)))
+       ))))
 
 (ert-deftest parse-command-line ()
   "Tests parsing of command lines"
-  (let ((f (compdb-source-file "foo.cpp" :command-line
-                               "clang -Dfoo -Dbar=baz -Uqux -I/opt/local/include -Iincludes -include bar.hpp main.cpp")))
+  (let ((f (compdb-entry "foo.cpp" :command-line
+                         "clang -Dfoo -Dbar=baz -Uqux -I/opt/local/include -Iincludes -include bar.hpp main.cpp")))
     (should (equal "clang" (oref f compiler)))
     (should (equal '(("foo") ("bar" . "baz")) (oref f defines)))
     (should (equal '("qux") (oref f undefines)))
--- ede-compdb.el
+++ ede-compdb.el
@@ -19,7 +19,7 @@
    )
   )
 
-(defclass compdb-source-file (eieio-named)
+(defclass compdb-entry (eieio-named)
   (
    (command-line :type string :initarg :command-line)
    (compiler :type string :initarg :compiler)
@@ -28,13 +28,19 @@
    (defines :type list :initarg :defines :initform '())
    (undefines :type list :initarg :undefines :initform '())
    )
-  "A source file in the compilation database"
+  "An entry in the compilation database"
   )
 
-(defmethod initialize-instance :AFTER ((this compdb-source-file) &rest fields)
+(defclass ede-compdb-target (ede-target)
+  (
+   (compilation :type compdb-entry :initarg :compilation)
+   )
+)
+
+(defmethod initialize-instance :AFTER ((this compdb-entry) &rest fields)
 
   ;; parse the command line if needed - compiler slot is used to determine whether we need to
-  (unless (slot-boundp this 'compiler)
+  (unless (slot-boundp this :compiler)
     ;; parsing code inspired by `command-line'
     (let ((args (split-string (oref this command-line)))
           (seenopt nil)
@@ -47,44 +53,67 @@
             (setq defval (match-string 4 argi))
             (setq argi (match-string 1 argi)))
           (cond
-           ((equal argi "-D") (object-add-to-list this 'defines (cons (or argval (pop args)) defval) t))
-           ((equal argi "-U") (object-add-to-list this 'undefines (or argval (pop args)) t))
-           ((member argi '("-I" "-F")) (object-add-to-list this 'include-path (or argval (pop args)) t))
-           ((equal argi "-include") (object-add-to-list this 'includes (pop args) t))
+           ((equal argi "-D") (object-add-to-list this :defines (cons (or argval (pop args)) defval) t))
+           ((equal argi "-U") (object-add-to-list this :undefines (or argval (pop args)) t))
+           ((member argi '("-I" "-F")) (object-add-to-list this :include-path (or argval (pop args)) t))
+           ((equal argi "-include") (object-add-to-list this :includes (pop args) t))
            ;; TODO: -nostdinc, -nostdlibinc, -nobuildinic
-           ((not seenopt) (oset this compiler (if (slot-boundp this 'compiler) (concat (oref this compiler) " " argi) argi)))
+           ((not seenopt) (oset this compiler (if (slot-boundp this :compiler) (concat (oref this compiler) " " argi) argi)))
            )
           (when (char-equal ?- (string-to-char argi))
             (setq seenopt t)
             )))
       )))
 
-(defmethod reload-compdb ((this ede-compdb-project))
+(defmethod project-rescan ((this ede-compdb-project))
+  "Reload the compilation database."
   (clrhash (oref this compdb))
   (mapcar (lambda (entry)
-            (let ((filename (cdr (assoc 'file entry)))
-                  (command-line (cdr (assoc 'command entry))))
-              (puthash filename (compdb-source-file filename :command-line command-line) (oref this compdb))))
+            (let* ((filename (cdr (assoc 'file entry)))
+                   (command-line (cdr (assoc 'command entry)))
+                   (target (if (slot-boundp this :targets) (object-assoc filename :path (oref this targets)) nil))
+                   (compilation (compdb-entry filename :command-line command-line)))
+              (puthash filename compilation (oref this compdb))
+              (when target
+                (oset this :compilation compilation))))
           (json-read-file (oref this file)))
-  )
 
-(defmethod reload-compdb-if-needed ((this ede-compdb-project))
-  (let* ((stats (file-attributes (oref this file)))
-         (size (nth 7 stats))
-         (mod (nth 5 stats)))
-    
+  (let ((stats (file-attributes (oref this file))))
+    (oset this file-timestamp (nth 5 stats))
+    (oset this file-size (nth 7 stats))))
+
+(defmethod project-rescan-if-needed ((this ede-compdb-project))
+  "Reload the compilation database if the corresponding watch file has changed."
+  (let ((stats (file-attributes (oref this file))))
     ;; Logic stolen from ede/arduino.el
     (when (or (not (oref this file-timestamp))
-              (/= (or (oref this file-size) 0) size)
-              (not (equal (oref this file-timestamp) mod)))
-      
-      (reload-compdb this)
-      (oset this file-timestamp mod)
-      (oset this file-size size)
-      )))
+              (/= (or (oref this file-size) 0) (nth 7 stats))
+              (not (equal (oref this file-timestamp) (nth 5 stats))))
+      (project-rescan this))))
 
 (defmethod initialize-instance :AFTER ((this ede-compdb-project) &rest fields)
-  (reload-compdb-if-needed this)
+  (unless (slot-boundp this 'targets)
+    (oset this :targets nil))  
+
+  (project-rescan-if-needed this)
   )
+
+(defmethod ede-system-include-path ((this ede-compdb-target))
+  "Get the system include path used by project THIS."
+  (oref (oref this compilation) include-path))
+
+(defmethod ede-find-target ((this ede-compdb-project) buffer)
+  "Find an EDE target in THIS for BUFFER.
+If one doesn't exist, create a new one."
+  (let* ((targets (oref this targets))
+         (file (expand-file-name (buffer-file-name buffer)))
+         (ans (object-assoc file :path targets))
+         )
+    (when (not ans)
+      (setq ans (ede-compdb-target file :compilation (gethash file (oref this compdb))))
+      (object-add-to-list this :targets ans)
+      )
+    ans))
+
 
 (provide 'ede-compdb)
