0d824b84536816b2775a669efece050ff866173c
Alastair Rankine <alastair@girtby.net>
2014-04-07 14:01:57 -0500

    Initial attempt at getting Travis-CI integration working

---
+++ manifest.pp
@@ -0,0 +1,31 @@
+# package { 'ninja-build':
+#   ensure => installed
+# }
+
+package { 'cmake':
+  ensure => installed
+}
+
+apt::ppa { 'ppa:cassou/emacs': }
+
+package { ['emacs24-common', 'emacs24-bin-common', 'emacs24-nox']:
+  ensure  => latest,
+  require => Apt::Ppa['ppa:cassou/emacs']
+}
+
+$cask_version = '0.6.0'
+$cask_archive = "cask-${cask_version}"
+
+archive { $cask_archive:
+  ensure        => present,
+  digest_type   => 'sha1',
+  digest_string => '6f836bb41f034d6be9611ee09c78eb8cc52b53ca',
+  url           => "https://github.com/cask/cask/archive/v${cask_version}.tar.gz",
+  target        => '/opt/',
+}
+
+file { '/usr/local/bin/cask':
+  ensure  => link,
+  target  => "/opt/${cask_archive}/bin/cask",
+  require => Archive[$cask_archive],
+}
